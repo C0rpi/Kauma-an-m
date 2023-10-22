a0ca40bb6459db16de347fed95d84f05c09b5f1e
Emil Eichhorn <emildereichhorn@gmail.com>
2023-10-22 18:07:45 +0200

    removed redundant code in json_parser

--- util/kauma_util.py
+++ util/kauma_util.py
@@ -7,10 +7,8 @@ def json_parser(input):
     data = json.loads(input)
     match data['action']:
         case 'bytenigma':
-            be_input_validation(data)
+            be_input_validation(data) 
             input = list(base64.b64decode(data['input']))
             rotors = data['rotors']
-            global reduction
-            reduction = len(rotors[0])-1
             output = bytenigma(input, rotors, len(rotors[0])-1)
     return json.dumps({"output": output})

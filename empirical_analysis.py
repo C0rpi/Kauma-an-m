066436738b24867a63a0eb1e3063925a2ac263ae
Emil Eichhorn <emildereichhorn@gmail.com>
2023-10-25 23:26:56 +0200

    randomized empirical analysis

--- empirical_analysis.py
+++ empirical_analysis.py
@@ -9,8 +9,9 @@ import random
 def show_bar():
     with open("tests/test1.json") as file: input = json.loads(file.read())
     rotors = input['rotors']
+    random.shuffle(rotors)
     out = b.bytenigma(list(b'\0'*2**20), rotors)
-    assert sha256(out).hexdigest() == '306a58f1d0589ec1ff4af1637e76774957389aa6152b6e04d6b389b1980efa8c'
+    #assert sha256(out).hexdigest() == '306a58f1d0589ec1ff4af1637e76774957389aa6152b6e04d6b389b1980efa8c'
     out = list(out)
     plot.hist(out,255)
     plot.show()

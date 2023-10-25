3558c95efc63e7f31ff86d163ed19cc5fb2488cc
Emil Eichhorn <emildereichhorn@gmail.com>
2023-10-25 21:41:55 +0200

    added file loading error handling

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from sys import argv
 from sys import stderr
 from sys import stdout
 if __name__ == "__main__":
+    
     if len(argv) ==2:
         output = json_parser(argv[1])
         stdout.write(output)
+
     else:
         stderr.write("wrong number of paramters, please input one json document")
     
--- util/kauma_util.py
+++ util/kauma_util.py
@@ -2,14 +2,24 @@ import json
 from bytenigma.bytenigma import be_input_validation
 from bytenigma.bytenigma import bytenigma
 import base64
+from sys import stderr
 
 def json_parser(json_path):
-    with open(json_path) as file: input = file.read()
-    data = json.loads(input)
+
+
+    try:
+        with open(json_path) as file: input = file.read()
+        data = json.loads(input)
+    except ValueError as error:
+        stderr.write(error)
+        stderr.write("Error opening the file by relativ and absolute path, aborting")
+        raise 
+
     match data['action']:
         case 'bytenigma':
             be_input_validation(data) 
             input = list(base64.b64decode(data['input']))
             rotors = data['rotors']
             output = bytenigma(input, rotors, len(rotors[0])-1)
+            
     return json.dumps({"output": output})

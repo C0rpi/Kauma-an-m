bf36e8d4c6bafc62e1c2eef6c4b4df2f6109c639
Emil Eichhorn <emildereichhorn@gmail.com>
2023-11-08 15:35:33 +0100

    rewrote parts of  client, added 2 new test cases   fixed the verfication for the first byte, modularized the code

--- Aufgabe2/client.py
+++ Aufgabe2/client.py
@@ -18,55 +18,50 @@ class Client:
         self.q = b''
 
     def run(self) -> bytes:
-        self.feld = int.to_bytes(0xff,2,'little')
+        self.feld = int.to_bytes(256,2,'little')
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((self.host, self.port)) #create_connection ist besser #create once and PRAY
 
             s.sendall(self.c)
             for j in range(16,0,-1):
+                #set padding values, length
                 pad = int.to_bytes(16-j+1)
                 pad_count = 16-j+1
-                self.q = b''
-                qbytes = [int.to_bytes(i,j) + bxor(self.dc,b'\0'*j + pad*(pad_count))[j:] for i in range(0xff+1)] #i love this 
-                for i in qbytes[:-1]:
-                    self.q = self.q + i
+                qbytes = self.generate_bytes(j,pad,pad_count) #i love this 
                 s.sendall(self.feld)
                 s.sendall(self.q)   
-                data = s.recv(256)
-                #ich hasse dass das notwendig ist
-                s.sendall(int.to_bytes(1,2,'little'))
-                s.sendall(qbytes[-1])   
-                data = data+ s.recv(1)
+                data = s.recv(256)  
+                #find the correct padding
                 for i,v in enumerate(data):
                     if v == 1:
+                        #set the byte value the server found to be acceptable padding
                         add = int.to_bytes((qbytes[i][j-1]))
-                        if j == 16: #verify solution for first byte
-                            s.sendall(b'\1\0')
+                        #if first byte, do cross check
+                        if j == 16:
                             verifier = qbytes[i][:j-2]+ int.to_bytes(qbytes[i][j-1] ^ 0xff) + add + qbytes[i][j:]
+                            s.sendall(b'\1\0')
                             s.send(verifier)
                             b = s.recv(1)
-                            if b ==b'\1':
-                                self.dc = self.dc[:j-1] + bxor(add,pad) + self.dc[j:]
-                                break
-                            else:
-                                continue
-                        else:
-                            self.dc = self.dc[:j-1] + bxor(add,pad) + self.dc[j:]
-                            break
+                            #if not valid: continue with next iteration, dont set dc
+                            if not b == b'\1':
+                                continue                        
+                        self.dc = self.dc[:j-1] + bxor(add,pad) + self.dc[j:]
+                        break
             ret = str(base64.b64encode(bxor(self.iv,self.dc)),'ascii')
+            print(f"check: {base64.b64decode(ret)}")
             return(ret)
-
+        
+    #generates the bytes that will be send to the server and return the 256*16byte values as a list
+    def generate_bytes(self,j,pad,pad_count):
+        self.q = b''
+        qbytes = [int.to_bytes(i,j) + bxor(self.dc,b'\0'*j + pad*(pad_count))[j:] for i in range(256)]
+        for i in qbytes:
+            self.q = self.q + i
+        return qbytes
 
 def bxor(ba : bytes,bb : bytes):
     return bytes(x ^ y for (x, y) in zip(ba, bb))     
 
-def verify_padding(input):
-    unpadder = PKCS7(128).unpadder()
-    try: 
-        unpadder.update(input)
-        unpadder.finalize()
-        unpadder = PKCS7(128).unpadder()
-        return True        
-    except ValueError:
-        return False
+c = Client("localhost",18732,base64.b64decode("RW1lcnNvbiBCcmFkeSACbw=="),base64.b64decode("AAAAAAAAAAAAAAAAAAAAAA=="))
+print(c.run())
